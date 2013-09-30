e3af3849f081a35c2d7b2347a2300fb1e221775a
laurabunea <laura.r.bunea@gmail.com>
2013-09-30 21:50:25 +1300

    updated some typos

--- www/index.php
+++ www/index.php
@@ -1,17 +1,68 @@
-<?php 
+<!doctype html>
+<html class='homepage'>
 
-	$page = 'home';
-    include 'header.php'; 
-?>      
-<!--add a theme class-->
-<div class='content clearfix homepage '>
-    <h1 class='caps' style='font-size:15em;'>F<span style='color:#E6DB74'>r</span>ed</h1>
-    <p class='intro'>
-        FRED the friendly FED takes the stress out of starting a new project... intro text some tips to get you started.. etc
-    </p>
-</div>
+    <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>FRED</title>
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link href="assets/css/prism.css" type="text/css" rel="stylesheet" />
+    </head>
 
+    <body class='clearfix homepage'>
+	    <!-- Top Navigation Bar - this holds the parent categories -->
+	    <div class='navigation--wrapper tb__quarternary'>
+	        <div class='navigation--top clearfix'>
+	           
 
-<?php 
-    include 'footer.php'; 
-?>
+	            <ul class='list--inline right'>
+	                <li> 
+	                    <a class='block--xs__stacked' href='/typography.php'>
+	                        Typography
+	                    </a>
+	                </li>
+	                <li>
+	                    <a class='block--xs__stacked' href='/layout.php'>
+	                        Layout
+	                    </a>
+	                </li>
+	                <li>
+	                    <a class='block--xs__stacked' href='/buttons.php'>
+	                        Buttons
+	                    </a>
+	                </li>
+	                <li>
+	                    <a class='block--xs__stacked' href='/themes.php'>
+	                        Themes
+	                    </a>
+	                </li>
+	                <li>
+	                    <a class='block--xs__stacked' href='/forms.php'>
+	                        Forms
+	                    </a>
+	                </li>
+	                <li>
+	                    <a class='block--xs__stacked' href='/mixins.php'>
+	                        Mixins
+	                    </a>
+	                </li>
+	                <li>
+	                    <a class='block--xs__stacked' href='/components.php'>
+	                        Components
+	                    </a>
+	                </li>
+	            </ul><!--list inline ends-->
+	        </div><!--navigation top ends-->
+	    </div><!--navigation wrapper ends-->
+	    <div class='content_wrapper clearfix'>   
+			<!--add a theme class-->
+			<div class='content  '>
+			    <h1 class='caps' style='font-size:15em;'>F<span style='color:#E6DB74'>r</span>ed</h1>
+			    <p class='intro'>
+			        FRED the friendly FED takes the stress out of starting a new project... intro text some tips to get you started.. etc
+			    </p>
+			</div>
+
+	    </div><!--content_wrapper ends-->
+        <script type="text/javascript" src="assets/includes/prism.js"></script>
+    </body>
+</html>
