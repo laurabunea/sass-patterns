c942d89955f28ef8f57b98cd5d4e309272087de5
laurabunea <laura.r.bunea@gmail.com>
2013-11-21 17:36:36 +1300

    cleaning up the copoy in layout.php

--- www/layout.php
+++ www/layout.php
@@ -352,123 +352,7 @@
         </div><!--  hello world block ends -->
     </div><!--layout-generic-block ends-->
     <hr class='mt--l'>
-    <!--Building blocks section starts-->
-    <div class='mt--l' id='responsiveAnchor1' >
-        <h2 class='h3'>
-            01. Building blocks - reponsive 
-        </h2>
-
-        <p>
-            These building blocks work in exactly the same way as the ones mentioned in <a href='#TODO'>layout</a>. 
-            The only difference is that they ...
-        </p>
-        <!--  code block starts -->
-        <div class=' block--s tb__tertiary'> 
-            <h4 class='tiny uppercase text-muted'>
-                scss 
-            </h4>
-            <pre>
-<code class="language-css">
-// Media queries breakpoints
-// -------------------------------------------------- 
-
-$mobile:                        30em;//Portrait regular mobiles//480px
-$skinny:                        48em;//Skinny 768px
-$desktop:                       60em;//Desktop 960px 
-$wide:                          75em;//Wide 1200px
-
-
-// Example one
-// --------------------------------------------------
-
-//margin-right  - desktop - default
-.mr--0          { margin-right:$bs--0; }
-
-//margin-right  - mobile
-.mr-m--0        { margin-right:$bs--0;}
-
-//margin-right  - skinny
-.mr-s--0        { margin-right:$bs--0;}
-
-//margin-right  - wide
-.mr-w--0        { margin-right:$bs--0;}
-
-
-// Example two
-// --------------------------------------------------
-
-//small block   - desktop - default
-.block--s       { margin-right:$bs--0; }
-
-//small block   - mobile
-.block-m--s     { margin-right:$bs--0;}
-
-//small block   - skinny
-.block-s--s     { margin-right:$bs--0;}
-
-//small block   - wide
-.block-w--s     { margin-right:$bs--0;}
-
-</code>
-            </pre>
-            <h4 class='tiny uppercase text-muted'>
-                code
-            </h4>
-            <pre>
-<code class="language-css">
-// Example one
-// -------------------------------------------------- 
-
-&lt;div class='block--m block-m--xs block-s--s block-w--l tb__green '&gt;
-    &lt;p&gt;
-        &lt;strong>desktop:&lt;/strong&gt;    block--m
-        &lt;strong>mobile:&lt;/strong&gt;     block-m--xs
-        &lt;strong>skinny:&lt;/strong&gt;     block-s--s
-        &lt;strong>wide:&lt;/strong&gt;       block-w--l
-    &lt;/p&gt;
-&lt;/div&gt;<!-- block xs ends-->
-
-// Example two
-// -------------------------------------------------- 
-&lt;div class='mt-m--xs mt-s--s mt--m mt-w--l block--s tb__green '&gt;
-    &lt;p&gt;
-        &lt;strong>mobile:&lt;/strong&gt; mt-m--xs
-        &lt;strong>skinny:&lt;/strong&gt; mt-s--s
-        &lt;strong>desktop:&lt;/strong&gt; mt--m
-        &lt;strong>wide:&lt;/strong&gt; mt-w--l
-    &lt;/p&gt;
-&lt;/div&gt;<!-- block xs ends-->
-
-</code>
-            </pre>
-        </div><!--  code block ends -->
-
-        <!--  hello world block starts -->
-        <div class='block--stacked block--s tb__secondary'>
-            <h4 class='tiny uppercase text-muted'>
-                hello world
-            </h4> 
-            <p>Example one</p>
-            <div class='block--m block-m--xs block-s--s block-w--l tb__green '>
-                <p>
-                    <strong>desktop:</strong>    block--m<br />
-                    <strong>mobile:</strong>     block-m--xs<br />
-                    <strong>skinny:</strong>     block-s--s<br />
-                    <strong>wide:</strong>       block-w--l
-                </p>
-            </div>
-            <p>Example two</p>
-            <div class='mt-m--xs mt-s--s mt--m mt-w--l block--s tb__green '>
-                <p>
-                    <strong>mobile:</strong> mt-m--xs<br />
-                    <strong>skinny:</strong> mt-s--s<br />
-                    <strong>desktop:</strong> mt--m<br />
-                    <strong>wide:</strong> mt-w--l
-                </p>
-            </div>
-            
-        </div><!--  hello world block ends -->
-    </div><!--Building blocks section ends-->
+   
     <!-- /****************************************  generic blocks  *******************************/ -->
     <div class='mt--l' id='layout-generic-block' >
         <h2 class='h3'>
@@ -546,8 +430,121 @@ $wide:                          75em;//Wide 1200px
             </div>-->
 
         </div><!--  hello world block ends -->
-    </div><!--layout-generic-block ends-->
+    
+        <h3 class='h4 mt--l'>
+             b) Section blocks - reponsive 
+        </h4>
 
+        <p>
+            These building blocks work in exactly the same way as the ones mentioned in <a href='#TODO'>layout</a>. 
+            The only difference is that they ...
+        </p>
+        <!--  code block starts -->
+        <div class=' block--s tb__tertiary'> 
+            <h4 class='tiny uppercase text-muted'>
+                scss 
+            </h4>
+            <pre>
+<code class="language-css">
+// Media queries breakpoints
+// -------------------------------------------------- 
+
+$mobile:                        30em;//Portrait regular mobiles//480px
+$skinny:                        48em;//Skinny 768px
+$desktop:                       60em;//Desktop 960px 
+$wide:                          75em;//Wide 1200px
+
+
+// Example one
+// --------------------------------------------------
+
+//margin-right  - desktop - default
+.mr--0          { margin-right:$bs--0; }
+
+//margin-right  - mobile
+.mr-m--0        { margin-right:$bs--0;}
+
+//margin-right  - skinny
+.mr-s--0        { margin-right:$bs--0;}
+
+//margin-right  - wide
+.mr-w--0        { margin-right:$bs--0;}
+
+
+// Example two
+// --------------------------------------------------
+
+//small block   - desktop - default
+.block--s       { margin-right:$bs--0; }
+
+//small block   - mobile
+.block-m--s     { margin-right:$bs--0;}
+
+//small block   - skinny
+.block-s--s     { margin-right:$bs--0;}
+
+//small block   - wide
+.block-w--s     { margin-right:$bs--0;}
+
+
+------------------------------------------------------- 
+    Here is the code for the example below:  
+------------------------------------------------------- 
+
+// Example one
+// -------------------------------------------------- 
+
+&lt;div class='block--m block-m--xs block-s--s block-w--l tb__green '&gt;
+    &lt;p&gt;
+        &lt;strong>desktop:&lt;/strong&gt;    block--m
+        &lt;strong>mobile:&lt;/strong&gt;     block-m--xs
+        &lt;strong>skinny:&lt;/strong&gt;     block-s--s
+        &lt;strong>wide:&lt;/strong&gt;       block-w--l
+    &lt;/p&gt;
+&lt;/div&gt;<!-- block xs ends-->
+
+// Example two
+// -------------------------------------------------- 
+&lt;div class='mt-m--xs mt-s--s mt--m mt-w--l block--s tb__green '&gt;
+    &lt;p&gt;
+        &lt;strong>mobile:&lt;/strong&gt; mt-m--xs
+        &lt;strong>skinny:&lt;/strong&gt; mt-s--s
+        &lt;strong>desktop:&lt;/strong&gt; mt--m
+        &lt;strong>wide:&lt;/strong&gt; mt-w--l
+    &lt;/p&gt;
+&lt;/div&gt;<!-- block xs ends-->
+
+</code>
+            </pre>
+        </div><!--  code block ends -->
+
+        <!--  hello world block starts -->
+        <div class='block--stacked block--s tb__secondary'>
+            <h4 class='tiny uppercase text-muted'>
+                hello world
+            </h4> 
+            <p>Example one</p>
+            <div class='block--m block-m--xs block-s--s block-w--l tb__green '>
+                <p>
+                    <strong>desktop:</strong>    block--m<br />
+                    <strong>mobile:</strong>     block-m--xs<br />
+                    <strong>skinny:</strong>     block-s--s<br />
+                    <strong>wide:</strong>       block-w--l
+                </p>
+            </div>
+            <p>Example two</p>
+            <div class='mt-m--xs mt-s--s mt--m mt-w--l block--s tb__green '>
+                <p>
+                    <strong>mobile:</strong> mt-m--xs<br />
+                    <strong>skinny:</strong> mt-s--s<br />
+                    <strong>desktop:</strong> mt--m<br />
+                    <strong>wide:</strong> mt-w--l
+                </p>
+            </div>
+            
+        </div><!--  hello world block ends -->
+    </div><!--Building blocks section ends-->
+    <hr class='mt--l' />
     <!-- /****************************************  Media blocks  *******************************/ -->
 
     <div class='mt--l' id='layout-media-block'>
@@ -602,12 +599,12 @@ $wide:                          75em;//Wide 1200px
                     </p>
                 </div>
             </div>
-
         </div><!--  hello world block ends -->
     </div><!-- layout-media-block ends-->
+    
     <hr class='mt--l'>
     <!-- /****************************************  Icon blocks  *******************************/ -->
-    <!-- hmm TODO check the code somethings not right-->
+    
     <div class='mt--l' id='layout-icon-blocks'>
         <h2  class='h3'>
            06. Icon blocks
