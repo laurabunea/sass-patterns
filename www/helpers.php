8349be8c98877c6fc8e8e7d56c7e576a53bb4d70
laurabunea <laura.r.bunea@gmail.com>
2013-11-29 11:34:55 +1300

    updated docs

---
+++ www/helpers.php
@@ -0,0 +1,23 @@
+<?php 
+    include 'header.php'; 
+?>
+<?php 
+    include 'helper-list.php'; 
+?>
+<div class='relative mb-l col-8-10 grid__item mt-xxxl ml-l' >
+    <div class='block-l pt-block' style='margin-left:50px'> 
+        <div class='heading'>
+            <h1>
+                Helper classes
+            </h1>
+        </div>
+        <p>
+            Intro
+        </p>
+    </div>
+</div>
+
+
+<?php 
+    include '../footer.php'; 
+?>
